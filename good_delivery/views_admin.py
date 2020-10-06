8e18cf65334fea5b8f51a1d2e676fdf51b164799
Francesco Filicetti <francesco.filicetti@unical.it>
2020-10-06 15:44:02 +0200

    only CSV to import

--- good_delivery/views_admin.py
+++ good_delivery/views_admin.py
@@ -15,45 +15,69 @@ def import_stockidentifiers_from_file(request):
     url = reverse('admin:good_delivery_deliverypointgoodstockidentifier_changelist')
 
     if request.method == 'POST' and request.FILES:
+
+        # check extension
+        if request.FILES['file_to_import'].content_type != 'text/csv':
+            messages.add_message(request, messages.ERROR, _("Only CSV"))
+            return HttpResponseRedirect(url)
+
+        # validate form
         form = AdminImportCSVForm(data=request.POST,
                                   files=request.FILES)
         if form.is_valid():
+
+            # get data
             campaign = form.cleaned_data['campaign']
             good = form.cleaned_data['good']
             file_to_import = form.cleaned_data['file_to_import']
 
+            # read CSV file
             decoded = file_to_import.read().decode(settings.DEFAULT_CHARSET)
             rows = decoded.split('\n')
 
+            # delivery points that don't exist
             disabled_delivery_points = []
             inserted = 0
             for row in rows:
                 if row:
                     values = row.split(',')
+
+                    # good identifier
                     identifier = values[0]
+                    # good notes
                     notes = values[1]
+                    # delivery point name
                     name = values[2]
 
+                    # if delivery point is not existent list
                     if name in disabled_delivery_points: continue
 
+                    # get delivery point object
                     delivery_point = DeliveryPoint.objects.filter(campaign=campaign,
                                                                   name=name).first()
+                    # if it doesn't exist, append to list and continue
                     if not delivery_point:
                         disabled_delivery_points.append(name)
                         messages.add_message(request, messages.ERROR,
                                              _("Delivery point {} inesistente.").format(name))
                         continue
 
+                    # get delivery point stock
                     stock = DeliveryPointGoodStock.objects.filter(delivery_point=delivery_point,
                                                                   good=good).first()
+
+                    # if delivery point hasn't got a stock, error and continue
                     if not stock:
                         disabled_delivery_points.append(name)
                         messages.add_message(request, messages.ERROR,
                                              _("Delivery point {} inesistente.").format(name))
                         continue
+
+                    # if current identifier already exists
                     existent_id = DeliveryPointGoodStockIdentifier.objects.filter(delivery_point_stock=stock,
                                                                                   good_identifier=identifier,
                                                                                   notes=notes).first()
+                    # no duplicates
                     if not existent_id:
                         stock_id = DeliveryPointGoodStockIdentifier(delivery_point_stock=stock,
                                                                     good_identifier=identifier,
