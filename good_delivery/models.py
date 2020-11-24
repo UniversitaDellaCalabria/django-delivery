658100e1c1a323a61f98830f7ff0bc438669d184
Francesco Filicetti <francesco.filicetti@unical.it>
2020-11-24 15:43:42 +0100

    phone in GoogDelivery

--- good_delivery/models.py
+++ good_delivery/models.py
@@ -244,6 +244,8 @@ class GoodDelivery(TimeStampedModel):
     address_number = models.CharField(max_length=100, blank=True, null=True)
     address_country = models.CharField(max_length=50, blank=True, null=True)
 
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
     notes = models.TextField(null=True, blank=True)
 
     class Meta:
